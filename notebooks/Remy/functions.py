f510ab5568f405fea8cb827de51aefdd1047080b
rlapere <remy.lapere.ensta@gmail.com>
2022-11-03 09:35:35 +0000

    ff

--- notebooks/Remy/functions.py
+++ notebooks/Remy/functions.py
@@ -14,25 +14,19 @@ def to_nc(indata, lat_test, lon_test, tts, fln):
     times = nc.createVariable('time', np.float64, ('time'))
     times[:] = tts
     nc.close()
-    
-    
+
 def compute_ivt(q,v,p):
     import numpy as np
     g = 9.81
     pres_ = np.copy(p)
     pres_[pres_<25000] = np.nan # 900hPa to 300hPa in theory but extended to include enough model levels
-    pres_[~np.isnan(pres_)] = 1.0
-    pres = np.where(pres_==1.0)
-    dp_ = np.diff(pres_)
-    print(dp_)
-    dp_=np.zeros(len(pres[0]))
-    for kk in range(len(dp_)):
-        dp_[kk] = -p[pres[0][0]+kk+1]+p[pres[0][0]+kk]
+    dp_ = np.append(np.diff(pres_),np.nan)
+    dp_[np.isnan(dp_)]=0.0
     dp_=np.repeat(dp_[np.newaxis,:],q.shape[0],axis=0)
     dp_=np.repeat(dp_[:,:,np.newaxis],q.shape[2],axis=2)
     dp_=np.repeat(dp_[:,:,:,np.newaxis],q.shape[3],axis=3)
     p_ = pres_
-    q_ = q[:,pres[0],:,:]
-    v_ = v[:,pres[0],:,:]
+    q_ = q
+    v_ = v
     iv_ = -1/g*np.sum(q_*v_*dp_,axis=1)
     return iv_

45988e4faef4e5cb1ba805e58eb7a5285165fb20
leahab <lea.haberstock@gmail.com>
2022-11-16 20:20:05 +0000

    writing se reporto

--- notebooks/Lea/all_functions.py
+++ notebooks/Lea/all_functions.py
@@ -164,10 +164,65 @@ def read_370(start_year, end_year):
 
     sopenlist=[s3.open(ss) for ss in s3path]
     bucket370_8500 = xr.open_mfdataset(sopenlist)).sel(time = slice(str(start_year)+"-01-01", str(end_year)+"-12-31")
+    
+                                                       return bucket370_8500
+                                                       
+def to_nc(indata, lat_test, lon_test, tts, fln):
+    from netCDF4 import Dataset
+    nc = Dataset(fln, 'w')
+    lat_dim = nc.createDimension('lat', indata.shape[1])
+    lon_dim = nc.createDimension('lon', indata.shape[2])
+    t_dim = nc.createDimension('time', indata.shape[0])
+    lat_var = nc.createVariable('lat', np.float64, ('lat'))
+    lat_var[:] = lat_test
+    lon_var = nc.createVariable('lon', np.float64, ('lon'))
+    lon_var[:] = lon_test
+    tnd = nc.createVariable('ivt', np.float64, ('time','lat','lon'))
+    tnd[:,:,:] = indata
+    times = nc.createVariable('time', np.float64, ('time'))
+    times[:] = tts
+    nc.close()
 
-    return bucket370_8500
+def compute_ivt(q,v,p):
+    import numpy as np
+    g = 9.81
+    pres_ = np.copy(p)
+    pres_[pres_<25000] = np.nan # 900hPa to 300hPa in theory but extended to include enough model levels
+    dp_ = np.append(np.diff(pres_),np.nan)
+    dp_[np.isnan(dp_)]=0.0
+    dp_=np.repeat(dp_[np.newaxis,:],q.shape[0],axis=0)
+    dp_=np.repeat(dp_[:,:,np.newaxis],q.shape[2],axis=2)
+    dp_=np.repeat(dp_[:,:,:,np.newaxis],q.shape[3],axis=3)
+    p_ = pres_
+    q_ = q
+    v_ = v
+    print(q)
+    print(v)
+    print(p_)
+    iv_ = -1/g*np.sum(q_*v_*dp_,axis=1)
+    return iv_
 
+                                                       
+def compute_threshold(start_year, end_year, experimentid):
+    dset = read_to_detect(start_year,end_year,experimentid)                                                       
+    vas = dset.va
+    hus = dset.hus
+    plev = dset.plev
+    lat_ = hus.lat
+    lon_ = hus.lon                                                   
+    
+    ivt = compute_ivt(hus,vas,plev)
+    ivt_ns = ivt.copy()
+    ivt_ns = xr.where(ivt_ns.lat<0,-ivt_ns,ivt_ns,True) # minus for southern hemisphere (positive toward the pole)
+    ivt_ns_pos = xr.where(ivt_ns<0,ivt_ns*0,ivt_ns,True) # negative values = not poleward
 
+    q93 = ivt_ns_pos.chunk(dict(time=-1)).quantile(0.93,'time')   
+    q93.to_netcdf('q93_2000.nc')
+    return print('finished computing; check if document q93_2000.nc was safed')   
+                                                       
+                                                       
+                                                       
+                                                       
 '''Data treatment'''
 
 ### bias correction ###
